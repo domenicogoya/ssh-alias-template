2e0c6044480c6b461622d4d093ebd0dc7665a59b
Pablo Domenico Goya Amato <alu0101557335@ull.edu.es>
2023-10-19 10:17:09 +0100

    Update execute.sh
    Changed e-mail.

--- auto/execute.sh
+++ auto/execute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/alu0101557335/ssh-alias-template/main/config
+wget https://raw.githubusercontent.com/domenicogoya/ssh-alias-template/main/config
 mv ./config ~/.ssh/config
